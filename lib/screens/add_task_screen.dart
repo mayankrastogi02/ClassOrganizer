8fc6bf66fe9d07298e9ce37f42491561490c13a6
Mayank Rastogi <mayankrustey2002@gmail.com>
2021-04-27 03:13:44 +0400

    Add Task Screen

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inti/intl.dart'
 
 class AddTaskScreen extends StatefulWidget {
   @override
@@ -10,6 +11,24 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   String _title = '';
   String _priority;
   DateTime _date = DateTime.now();
+  TextEditingController _dateController = TextEditingController();
+
+  final DateFormat _dateFormatter = DateFormat('MMMM dd, yyyy');
+
+  _handleDatePicker() async {
+    final DateTime date = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(2021),
+      lastDate: DateTime(2100),
+    );
+    if (date != null && date != _date) {
+      setState(() {
+        _date = date;
+      });
+      _dateController.text = _date.toString();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,19 +58,56 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
             key: _formKey,
             child: Column(
               children: <Widget>[
-                TextFormField(
-                  style: TextStyle(fontSize: 10.0),
-                  decoration: InputDecoration(
-                    labelText: 'Title',
-                    labelStyle: TextStyle(fontSize: 18.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextFormField(
+                    style: TextStyle(fontSize: 10.0),
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      labelStyle: TextStyle(fontSize: 18.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    validator: (input) => input.trim().isEmpty
+                        ? 'Please enter a task title'
+                        : null,
+                    onSaved: (input) => _title = input,
+                    initialValue: _title,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextFormField(
+                    controller: _dateController,
+                    style: TextStyle(fontSize: 10.0),
+                    onTap: _handleDatePicker,
+                    decoration: InputDecoration(
+                      labelText: 'Date',
+                      labelStyle: TextStyle(fontSize: 18.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
                     ),
                   ),
-                  validator: (input) =>
-                      input.trim().isEmpty ? 'Please enter a task title' : null,
-                  onSaved: (input) => _title = input,
-                  initialValue: _title,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextFormField(
+                    style: TextStyle(fontSize: 10.0),
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      labelStyle: TextStyle(fontSize: 18.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    validator: (input) => input.trim().isEmpty
+                        ? 'Please enter a task title'
+                        : null,
+                    onSaved: (input) => _title = input,
+                    initialValue: _title,
+                  ),
                 ),
               ],
             ),
