fb6a2f0fe041ae288335ffac02e9266ee5c79078
Mayank Rastogi <mayankrustey2002@gmail.com>
2021-04-27 02:11:13 +0400

    Home Page + Task Screen

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatefulWidget {
+  @override
+  _AddTaskScreenState createState() => _AddTaskScreenState();
+}
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}
---
+++ lib/screens/todo_list_screen.dart
@@ -0,0 +1,77 @@
+import 'package:class_organizer/screens/add_task_screen.dart';
+import 'package:flutter/material.dart';
+
+class ToDoListScreen extends StatefulWidget {
+  @override
+  _ToDoListScreenState createState() => _ToDoListScreenState();
+}
+
+class _ToDoListScreenState extends State<ToDoListScreen> {
+  Widget _buildTask(int index) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('Task Title'),
+            subtitle: Text('Apr 28, 2021 • High'),
+            trailing: Checkbox(
+              onChanged: (value) {
+                print(value);
+              },
+              activeColor: Theme.of(context).primaryColor,
+              value: true,
+            ),
+          ),
+          const Divider(
+            color: Colors.black,
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Theme.of(context).primaryColor,
+          child: Icon(Icons.add),
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => AddTaskScreen(),
+            ),
+          ),
+        ),
+        body: ListView.builder(
+            padding: EdgeInsets.symmetric(vertical: 80.0),
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == 0) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('My Tasks',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      SizedBox(height: 10.0),
+                      Text('1 of 10',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w600,
+                          )),
+                    ],
+                  ),
+                );
+              }
+              return _buildTask(index);
+            }));
+  }
+}
