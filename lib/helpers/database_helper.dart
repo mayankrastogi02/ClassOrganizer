8c2d16c2a8ef9a8bbd0bc3487ec8a76edfdafb6f
Mayank Rastogi <mayankrustey2002@gmail.com>
2021-04-27 15:47:04 +0400

    Added database implementation

--- lib/helpers/database_helper.dart
+++ lib/helpers/database_helper.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
@@ -5,4 +7,38 @@ class DatabaseHelper {
   static Database _db;
 
   DatabaseHelper._instance();
+
+  String tasksTable = 'task_table';
+  String colId = 'id';
+  String colTitle = 'title';
+  String colDate = 'date';
+  String colPriority = 'priority';
+  String colStatus = 'Status';
+
+  // Task Table
+  // Id | Title | Date | Priority | Status
+  // 0      ''     ''       ''        ''
+  // 1      ''     ''       ''        ''
+  // 2      ''     ''       ''        ''
+
+  //getter
+  Future<Database> get db async {
+    if (_db == null) {
+      _db = await _initDb();
+    }
+    return _db;
+  }
+
+  Future<Database> _initDb() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path = dir.path + 'todo_list.db'; //Second part is nname of database
+    final todoListDb =
+        await openDatabase(path, version: 1, onCreate: _createDb);
+    return todoListDb;
+  }
+
+  void _createDb(Database db, int version) async {
+    await db.execute(
+        "CREATE TABLE $tasksTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, $colDate TEXT, $colPriority TEXT, $colStatus INTEGER)");
+  }
 }
